bcf7566ab99f1ccf620553b6b4001eab5b9dfcf5
Nelly Hateva <nelly.hateva@gmail.com>
2014-09-22 21:32:24 +0300

    change the code layout, remove reading of the dict, make some variables global and add some new functons

--- src/main.c
+++ src/main.c
@@ -1,17 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "automaton.h"
 
 
 int main(int argc, char* argv[])
 {
-    if (argc != 3)
-    {
-        printf("cmt: No file names given\n");
-        exit(EXIT_FAILURE);
-    }
-
-    create_minimal_transducer_for_given_list(argv[1], argv[2]);
-
+    dispatch(argc, argv);
     return 0;
 }
